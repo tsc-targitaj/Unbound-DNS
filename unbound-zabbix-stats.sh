eb03b76bb128574f5ce6869f193473881c790f83
Sergey Tekuchev <tsc-targitaj@yandex.ru>
2023-04-12 12:01:46 +0300

    Commit

--- unbound-zabbix-stats.sh
+++ unbound-zabbix-stats.sh
@@ -14,6 +14,8 @@ fi
 IP_ZABBIX=$1
 # NAME Unbound on Zabbix
 NAME_HOST=$2
+#ZABBIX_OPTS="-z ${IP_ZABBIX} -s ${NAME_HOST}"
+ZABBIX_OPTS="$1"
 DIR_TEMP=/var/tmp/
 FILE="${DIR_TEMP}dump_unbound_control_stats.txt"
 unbound-control stats > ${FILE}
@@ -50,33 +52,33 @@ NUM_ANSWER_RCODE_nodata=$(grep -w 'num.answer.rcode.nodata' ${FILE} | cut -d '='
 NUM_ANSWER_secure=$(grep -w 'num.answer.secure' ${FILE} | cut -d '=' -f2)
 
 #	Sending info to zabbix_server, if variables is not empty!
-[ -z ${TOTAL_NUM_QUERIES} ] ||  zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.num.queries -o ${TOTAL_NUM_QUERIES}
-[ -z ${TOTAL_NUM_CACHEHITS} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.num.cachehits -o ${TOTAL_NUM_CACHEHITS}
-[ -z ${TOTAL_NUM_CACHEMISS} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.num.cachemiss -o ${TOTAL_NUM_CACHEMISS}
-[ -z ${TOTAL_NUM_PREFETCH} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.num.prefetch -o ${TOTAL_NUM_PREFETCH}
-[ -z ${TOTAL_NUM_RECURSIVEREPLIES} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.num.recursivereplies -o ${TOTAL_NUM_RECURSIVEREPLIES}
+[ -z ${TOTAL_NUM_QUERIES} ] ||  zabbix_sender $ZABBIX_OPTS -k total.num.queries -o ${TOTAL_NUM_QUERIES}
+[ -z ${TOTAL_NUM_CACHEHITS} ] || zabbix_sender $ZABBIX_OPTS -k total.num.cachehits -o ${TOTAL_NUM_CACHEHITS}
+[ -z ${TOTAL_NUM_CACHEMISS} ] || zabbix_sender $ZABBIX_OPTS -k total.num.cachemiss -o ${TOTAL_NUM_CACHEMISS}
+[ -z ${TOTAL_NUM_PREFETCH} ] || zabbix_sender $ZABBIX_OPTS -k total.num.prefetch -o ${TOTAL_NUM_PREFETCH}
+[ -z ${TOTAL_NUM_RECURSIVEREPLIES} ] || zabbix_sender $ZABBIX_OPTS -k total.num.recursivereplies -o ${TOTAL_NUM_RECURSIVEREPLIES}
 
-[ -z ${TOTAL_REQ_MAX} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.requestlist.max -o ${TOTAL_REQ_MAX}
-[ -z ${TOTAL_REQ_AVG} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.requestlist.avg -o ${TOTAL_REQ_AVG}
-[ -z ${TOTAL_REQ_OVERWRITTEN} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.requestlist.overwritten -o ${TOTAL_REQ_OVERWRITTEN}
-[ -z ${TOTAL_REQ_EXCEEDED} ] ||  zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.requestlist.exceeded -o ${TOTAL_REQ_EXCEEDED}
-[ -z ${TOTAL_REQ_CURRENT_ALL} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.requestlist.current.all -o ${TOTAL_REQ_CURRENT_ALL}
-[ -z ${TOTAL_REQ_CURRENT_USER} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.requestlist.current.user -o ${TOTAL_REQ_CURRENT_USER}
+[ -z ${TOTAL_REQ_MAX} ] || zabbix_sender $ZABBIX_OPTS -k total.requestlist.max -o ${TOTAL_REQ_MAX}
+[ -z ${TOTAL_REQ_AVG} ] || zabbix_sender $ZABBIX_OPTS -k total.requestlist.avg -o ${TOTAL_REQ_AVG}
+[ -z ${TOTAL_REQ_OVERWRITTEN} ] || zabbix_sender $ZABBIX_OPTS -k total.requestlist.overwritten -o ${TOTAL_REQ_OVERWRITTEN}
+[ -z ${TOTAL_REQ_EXCEEDED} ] ||  zabbix_sender $ZABBIX_OPTS -k total.requestlist.exceeded -o ${TOTAL_REQ_EXCEEDED}
+[ -z ${TOTAL_REQ_CURRENT_ALL} ] || zabbix_sender $ZABBIX_OPTS -k total.requestlist.current.all -o ${TOTAL_REQ_CURRENT_ALL}
+[ -z ${TOTAL_REQ_CURRENT_USER} ] || zabbix_sender $ZABBIX_OPTS -k total.requestlist.current.user -o ${TOTAL_REQ_CURRENT_USER}
 
-[ -z ${TOTAL_TCPUSAGE} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k total.tcpusage -o ${TOTAL_TCPUSAGE}
+[ -z ${TOTAL_TCPUSAGE} ] || zabbix_sender $ZABBIX_OPTS -k total.tcpusage -o ${TOTAL_TCPUSAGE}
 
-[ -z ${NUM_QUERY_TYPE_A} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.query.a -o ${NUM_QUERY_TYPE_A}
-[ -z ${NUM_QUERY_TYPE_NS} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.query.ns -o ${NUM_QUERY_TYPE_NS}
-[ -z ${NUM_QUERY_TYPE_MX} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.query.mx -o ${NUM_QUERY_TYPE_MX}
-[ -z ${NUM_QUERY_TYPE_TXT} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.query.txt -o ${NUM_QUERY_TYPE_TXT}
-[ -z ${NUM_QUERY_TYPE_PTR} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.query.ptr -o ${NUM_QUERY_TYPE_PTR}
-[ -z ${NUM_QUERY_TYPE_AAAA} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.query.aaaa -o ${NUM_QUERY_TYPE_AAAA}
-[ -z ${NUM_QUERY_TYPE_SRV} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.query.srv -o ${NUM_QUERY_TYPE_SRV}
-[ -z ${NUM_QUERY_TYPE_SOA} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.query.soa -o ${NUM_QUERY_TYPE_SOA}
+[ -z ${NUM_QUERY_TYPE_A} ] || zabbix_sender $ZABBIX_OPTS -k num.query.a -o ${NUM_QUERY_TYPE_A}
+[ -z ${NUM_QUERY_TYPE_NS} ] || zabbix_sender $ZABBIX_OPTS -k num.query.ns -o ${NUM_QUERY_TYPE_NS}
+[ -z ${NUM_QUERY_TYPE_MX} ] || zabbix_sender $ZABBIX_OPTS -k num.query.mx -o ${NUM_QUERY_TYPE_MX}
+[ -z ${NUM_QUERY_TYPE_TXT} ] || zabbix_sender $ZABBIX_OPTS -k num.query.txt -o ${NUM_QUERY_TYPE_TXT}
+[ -z ${NUM_QUERY_TYPE_PTR} ] || zabbix_sender $ZABBIX_OPTS -k num.query.ptr -o ${NUM_QUERY_TYPE_PTR}
+[ -z ${NUM_QUERY_TYPE_AAAA} ] || zabbix_sender $ZABBIX_OPTS -k num.query.aaaa -o ${NUM_QUERY_TYPE_AAAA}
+[ -z ${NUM_QUERY_TYPE_SRV} ] || zabbix_sender $ZABBIX_OPTS -k num.query.srv -o ${NUM_QUERY_TYPE_SRV}
+[ -z ${NUM_QUERY_TYPE_SOA} ] || zabbix_sender $ZABBIX_OPTS -k num.query.soa -o ${NUM_QUERY_TYPE_SOA}
 
-[ -z ${NUM_ANSWER_RCODE_NOERROR} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.answer.rcode.NOERROR -o ${NUM_ANSWER_RCODE_NOERROR}
-[ -z ${NUM_ANSWER_RCODE_NXDOMAIN} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.answer.rcode.NXDOMAIN -o ${NUM_ANSWER_RCODE_NXDOMAIN}
-[ -z ${NUM_ANSWER_RCODE_SERVFAIL} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.answer.rcode.SERVFAIL -o ${NUM_ANSWER_RCODE_SERVFAIL}
-[ -z ${NUM_ANSWER_RCODE_REFUSED} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.answer.rcode.REFUSED -o ${NUM_ANSWER_RCODE_REFUSED}
-[ -z ${NUM_ANSWER_RCODE_nodata} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.answer.rcode.nodata -o ${NUM_ANSWER_RCODE_nodata}
-[ -z ${NUM_ANSWER_secure} ] || zabbix_sender -z ${IP_ZABBIX} -s ${NAME_HOST} -k num.answer.secure -o ${NUM_ANSWER_secure}
+[ -z ${NUM_ANSWER_RCODE_NOERROR} ] || zabbix_sender $ZABBIX_OPTS -k num.answer.rcode.NOERROR -o ${NUM_ANSWER_RCODE_NOERROR}
+[ -z ${NUM_ANSWER_RCODE_NXDOMAIN} ] || zabbix_sender $ZABBIX_OPTS -k num.answer.rcode.NXDOMAIN -o ${NUM_ANSWER_RCODE_NXDOMAIN}
+[ -z ${NUM_ANSWER_RCODE_SERVFAIL} ] || zabbix_sender $ZABBIX_OPTS -k num.answer.rcode.SERVFAIL -o ${NUM_ANSWER_RCODE_SERVFAIL}
+[ -z ${NUM_ANSWER_RCODE_REFUSED} ] || zabbix_sender $ZABBIX_OPTS -k num.answer.rcode.REFUSED -o ${NUM_ANSWER_RCODE_REFUSED}
+[ -z ${NUM_ANSWER_RCODE_nodata} ] || zabbix_sender $ZABBIX_OPTS -k num.answer.rcode.nodata -o ${NUM_ANSWER_RCODE_nodata}
+[ -z ${NUM_ANSWER_secure} ] || zabbix_sender $ZABBIX_OPTS -k num.answer.secure -o ${NUM_ANSWER_secure}
